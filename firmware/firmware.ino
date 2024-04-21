dd4a1c0e2d3daecb89299af0523b0127b13e0f74
Thorsten Lanfer <tlanfer@gmail.com>
2024-04-21 13:43:56 +0200

    Convert to esp32

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,50 +1,77 @@
 #include <FS.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <ESPAsyncWiFiManager.h>
-#include <SPIFFSEditor.h>
-#include <AsyncElegantOTA.h>
-#include <Hash.h>
-#include <elegantWebpage.h>
+#include <WiFi.h>
+#include <LittleFS.h>
+#include <AsyncFsWebServer.h>
+#include <ESPmDNS.h>
 
 #define HOSTNAME "squirtttv"
 
 #define ADMIN_USERNAME "admin"
 #define ADMIN_PASSWORD "admin"
 
-#define PUMP_PIN 12
+#define PUMP_PIN D4
+#define FORMAT_LITTLEFS_IF_FAILED true
 
-AsyncWebServer server(80);
-DNSServer dns;
+AsyncFsWebServer server(80, LittleFS, "myServer");
+bool captiveRun = false;
 
 int del = 0;
 
-void setupOutput(){
+void setupOutput() {
   pinMode(PUMP_PIN, OUTPUT);
   digitalWrite(PUMP_PIN, LOW);
 }
 
-void setupSerial(){
+void setupSerial() {
   Serial.begin(115200);
   Serial.println("Serial ready");
 }
 
-void setupWifi(){
-  AsyncWiFiManager wifiManager(&server,&dns);
-  wifiManager.autoConnect(HOSTNAME);
-  
-  Serial.print("Wifi ready: ");
-  Serial.println(WiFi.localIP());
+void setupFS(){
+  if (LittleFS.begin()){
+    File root = LittleFS.open("/", "r");
+    File file = root.openNextFile();
+    while (file){
+      Serial.printf("FS File: %s, size: %d\n", file.name(), file.size());
+      file = root.openNextFile();
+    }
+  } else {
+    Serial.println("ERROR on mounting filesystem. It will be reformatted!");
+    LittleFS.format();
+    ESP.restart();
+  }
+  Serial.println("FS ready");
 }
 
-void setupWebserver(){
-  SPIFFS.begin();
-  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
-  server.addHandler(new SPIFFSEditor(ADMIN_USERNAME,ADMIN_PASSWORD));
+void getFsInfo(fsInfo_t* fsInfo) {
+	fsInfo->fsName = "LittleFS";
+	fsInfo->totalBytes = LittleFS.totalBytes();
+	fsInfo->usedBytes = LittleFS.usedBytes();  
+}
 
-  server.on("/squirt", HTTP_GET, [] (AsyncWebServerRequest *request) {
+void setupWebserver() {
+  IPAddress myIP = server.startWiFi(15000);
+  if (!myIP) {
+    Serial.println("\n\nNo WiFi connection, start AP and Captive Portal\n");
+    myIP = WiFi.softAPIP();
+    Serial.print("My IP 1 ");
+    Serial.println(myIP.toString());
+    server.startCaptivePortal("ESP_AP", "123456789", "/setup");
+    myIP = WiFi.softAPIP();
+    Serial.print("\nMy IP 2 ");
+    Serial.println(myIP.toString());
+  }
+  server.setSetupPageTitle("Simple Async FS Captive Web Server");
+  server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
+
+  server.on("/identify", HTTP_GET, [](AsyncWebServerRequest *request) {
+    AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", "This is a streamer squirter");
+    response->addHeader("Server", "squirtttv/2.0");
+
+    request->send(response);
+  });
+
+  server.on("/squirt", HTTP_GET, [](AsyncWebServerRequest *request) {
     if (request->hasParam("duration")) {
       int duration = request->getParam("duration")->value().toInt();
       del = duration;
@@ -52,47 +79,55 @@ void setupWebserver(){
     request->redirect("/");
   });
 
-  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
-
-  server.onNotFound([](AsyncWebServerRequest *request){
+  server.onNotFound([](AsyncWebServerRequest *request) {
     request->redirect("https://github.com/tlanfer/squirtttv");
   });
 
-  
-  server.begin();
+  server.enableFsCodeEditor();
+  server.setFsInfoCallback(getFsInfo);
+
+  server.init();
   Serial.println("Webserver ready");
 }
 
 void setupMdns(){
-  if (!MDNS.begin(HOSTNAME)) {
-    Serial.println("Error setting up MDNS responder!");
+  if (!MDNS.begin("esp32")) {
+      Serial.println("Error setting up MDNS responder!");
+      while(1) {
+          delay(1000);
+      }
   }
-  MDNS.addService(HOSTNAME, "tcp", 80); // Announce esp tcp service on port 8080
   
-  Serial.println("mDNS ready");
+  MDNS.addService("squirtttv", "tcp", 80);
 }
 
 void setup() {
   setupOutput();
   setupSerial();
-  setupWifi();
+  setupFS();
   setupWebserver();
   setupMdns();
 }
 
 void squirt(int t) {
   digitalWrite(PUMP_PIN, HIGH);
-  delay(t);
+  int squirtTo = millis() + t;
+  while (millis() < squirtTo){
+    delay(1);
+  }
   digitalWrite(PUMP_PIN, LOW);
 }
 
 void loop() {
-  MDNS.update();
-  if( del > 0) {
+  if (captiveRun)
+    server.updateDNS();
+  
+  if (del > 0) {
     Serial.print("Squirt for ");
     Serial.print(del);
     Serial.println("ms");
     squirt(del);
+    Serial.println("done squirting");
     del = 0;
   }
 }
